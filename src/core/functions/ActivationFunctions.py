d2e9d080e8e387f5dca78e2bc4b913000a53a8ec
WilliamsASF <willv2299@gmail.com>
2025-01-06 18:27:48 -0300

    Implementações das funções de ativações(linear,softplus e softmax)

--- src/core/functions/ActivationFunctions.py
+++ src/core/functions/ActivationFunctions.py
@@ -2,21 +2,46 @@
 import numpy as np
 
 class ActivationFunctions():
+
+#Função de ativação para regressão:
+
+    def linear(x):
+        return x
     
-    def tanh(x):
-        return np.tanh(x)
-    
-    def tanh_prime(x):
-        return 1 - np.tanh(x) ** 2
-    
-    def sigmoid(x):
-        return 1 / (1 + np.exp(-x))
-    
-    def sigmoid_prime(x):
-        return (1 / (1 + np.exp(-x))) * (1 - 1 / (1 + np.exp(-x)))
+    def linear_prime(x):
+        return np.ones_like(x)
     
     def relu(x):
         return np.maximum(0, x)
     
     def relu_prime(x):
         return np.where(x > 0, 1, 0)
+    
+    def softplus(x):
+        return np.log(1 + np.exp(x))
+    
+    def softplus_prime(x):
+        return 1 / (1 + np.exp(-x))
+    
+
+#Função de ativação para classificação:    
+
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+    
+    def sigmoid_prime(x):
+        return (1 / (1 + np.exp(-x))) * (1 - 1 / (1 + np.exp(-x)))
+    
+    def softmax(x):
+        exp_x = np.exp(x - np.max(x))  
+        return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
+    
+    def softmax_prime(x):
+        s = ActivationFunctions.softmax(x).reshape(-1, 1)
+        return np.diagflat(s) - np.dot(s, s.T)   
+    
+    def tanh(x):
+        return np.tanh(x)
+    
+    def tanh_prime(x):
+        return 1 - np.tanh(x) ** 2
