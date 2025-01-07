187ebffc5b41968f43908b6b4d3838f715077cfe
WilliamsASF <willv2299@gmail.com>
2025-01-07 17:53:28 -0300

    Adição das loss functions

--- src/core/functions/LossFunctions.py
+++ src/core/functions/LossFunctions.py
@@ -2,8 +2,41 @@ import numpy as np
 
 class LossFunctions():
 
+    @staticmethod
     def mse(y_true , y_pred):
         return np.mean(np.power(y_true - y_pred, 2))
-    
+    @staticmethod
     def mse_prime(y_true, y_pred):
         return 2 * (y_pred - y_true) / y_true.size
+
+    @staticmethod
+    def log_likelihood(y_true, y_pred):
+        y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
+        return -np.log(y_pred[np.arange(len(y_true)), y_true.argmax(axis=1)])
+
+    @staticmethod
+    def log_likelihood_prime(y_true, y_pred):
+        y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
+        grad = np.zeros_like(y_pred)
+        grad[np.arange(len(y_true)), y_true.argmax(axis=1)] = -1 / y_pred[np.arange(len(y_true)), y_true.argmax(axis=1)]
+        return grad / len(y_true)
+
+    @staticmethod
+    def binary_cross_entropy(y_true, y_pred):
+        y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)  
+        return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+    @staticmethod
+    def binary_cross_entropy_prime(y_true, y_pred):
+        y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
+        return (y_pred - y_true) / (y_pred * (1 - y_pred) * y_true.size)
+
+    @staticmethod
+    def cross_entropy(y_true, y_pred):
+        y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15) 
+        return -np.sum(y_true * np.log(y_pred)) / y_true.shape[0]
+
+    @staticmethod
+    def cross_entropy_prime(y_true, y_pred):
+        y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
+        return -y_true / y_pred / y_true.shape[0]
